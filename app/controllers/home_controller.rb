a57392a819227726d791bc0a636cdfcb1cb0a603
aedeeley <aedeeley@gmail.com>
2018-12-03 07:44:52 -0500

    organize/style jobs output, style app as a whole, implement hover.css, clean up code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @jobs = Job.all.order("created_at desc")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   resources :jobs
   devise_for :users
-  root to: 'jobs#index'
-  
-  root to: "main#index"
+  root to: 'home#index'
 
   resources :search, only: [:index]
 end
